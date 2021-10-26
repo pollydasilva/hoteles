0e103e39f571b3c8d4279c6af7264621dd24f534
Paula <paula.dasilva.arriagada@gmail.com>
2021-10-26 12:02:02 -0300

    agregando migraciones a la base de datos

--- README.md
+++ README.md
@@ -1,24 +1,14 @@
 # README
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+Enunciado
 
-Things you may want to cover:
+Una nueva comunidad de pequeños hoteles campestres desean automatizar las reservas para sus diferentes habitaciones y quisieran hacerlo a través de internet. Para ejemplificar el uso del mismo se decidió pensar en las siguientes acciones:
 
-* Ruby version
+Registrar hoteles con sus nombre y cantidad de estrellasRegistrar habitaciones relacionadas a solo 1 hotel, que tengan un nombre o número así como el precio y también saber si está reservada o no.
+Un usuario que ingrese pueda navegar buscando hoteles y reservar una habitación que le interese, para ellos debería tener un nombre, número de identificación y teléfono.
 
-* System dependencies
+Cosas deseables del software serían las siguientes:
 
-* Configuration
-
-* Database creation
-
-* Database initialization
-
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
+Al momento de registrar usuarios, que puedan iniciar sesión.
+Controlar el registro de hoteles y habitaciones con un usuario administrador para eso
+Tener un buscador de hoteles por el nombre en la página principal
---
+++ db/schema.rb
@@ -0,0 +1,74 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_10_26_135625) do
+
+  create_table "ciudades", force: :cascade do |t|
+    t.string "nombre"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "habitaciones", force: :cascade do |t|
+    t.string "nombre"
+    t.boolean "esta_reservada"
+    t.integer "precio"
+    t.integer "hotel_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["hotel_id"], name: "index_habitaciones_on_hotel_id"
+  end
+
+  create_table "hoteles", force: :cascade do |t|
+    t.string "nombre"
+    t.integer "estrellas"
+    t.integer "ciudad_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["ciudad_id"], name: "index_hoteles_on_ciudad_id"
+  end
+
+  create_table "reservas", force: :cascade do |t|
+    t.integer "habitacion_id", null: false
+    t.integer "usuario_id", null: false
+    t.date "fecha_ingreso"
+    t.date "fecha_salida"
+    t.string "codigo"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["habitacion_id"], name: "index_reservas_on_habitacion_id"
+    t.index ["usuario_id"], name: "index_reservas_on_usuario_id"
+  end
+
+  create_table "roles", force: :cascade do |t|
+    t.string "rol"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "usuarios", force: :cascade do |t|
+    t.string "nombre"
+    t.string "identificacion"
+    t.string "telefono"
+    t.string "password_digest"
+    t.integer "rol_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["rol_id"], name: "index_usuarios_on_rol_id"
+  end
+
+  add_foreign_key "habitaciones", "hoteles"
+  add_foreign_key "hoteles", "ciudades"
+  add_foreign_key "reservas", "habitaciones"
+  add_foreign_key "reservas", "usuarios"
+  add_foreign_key "usuarios", "roles"
+end
